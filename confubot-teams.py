57e06b3e93a41dd4f3b2b7d4015ae7ae1f16fec1
richardporras <richard.porras@gmail.com>
2025-03-03 17:22:58 +0100

    Update confubot-teams.py

--- confubot-teams.py
+++ confubot-teams.py
@@ -1,13 +1,14 @@
 import os
-from flask import Flask
+
+from flask import (Flask, redirect, render_template, request,
+                   send_from_directory, url_for)
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    print('Request for index page received')
-    return "✅ Flask app is running on Azure!"
+@app.route('/')
+def index():
+   print('Request for index page received')
+   return "Flask app is running on Azure!"
 
-if __name__ == "__main__":
-    port = int(os.getenv("PORT", 8000))  # Azure proporciona el puerto dinámico
-    app.run(host="0.0.0.0", port=port, debug=True)
+if __name__ == '__main__':
+   app.run()
