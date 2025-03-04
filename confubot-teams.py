5c519783087f51756f97005a72a55de41ae62dfc
richardporras <richard.porras@gmail.com>
2025-03-04 12:11:51 +0100

    Update confubot-teams.py

--- confubot-teams.py
+++ confubot-teams.py
@@ -10,5 +10,10 @@ def index():
    print('Request for index page received')
    return "OK"
 
+@app.route('/ping')
+def index():
+   print('Request for ping')
+   return "Pong"
+
 if __name__ == '__main__':
    app.run()
