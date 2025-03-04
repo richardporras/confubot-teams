b711864427610aa57d8200bf8d42e745f2b923c5
richardporras <richard.porras@gmail.com>
2025-03-04 11:07:40 +0100

    Update confubot-teams.py

--- confubot-teams.py
+++ confubot-teams.py
@@ -5,10 +5,11 @@ from flask import (Flask, redirect, render_template, request,
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
    print('Request for index page received')
-   return "Flask app is running on Azure!"
+   return "OK"
 
 if __name__ == '__main__':
    app.run()
